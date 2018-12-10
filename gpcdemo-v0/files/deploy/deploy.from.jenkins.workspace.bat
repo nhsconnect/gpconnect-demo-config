f3c4a7f409da04bf59b4e7f1d429f0a99c05b2e3
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2018-12-10 21:17:15 +0000

    Remove SQL renaming as database names are now handling correctly by demonstrator code

--- gpcdemo-v0/files/deploy/deploy.from.jenkins.workspace.bat
+++ gpcdemo-v0/files/deploy/deploy.from.jenkins.workspace.bat
@@ -6,7 +6,4 @@ set GPC_V0_HOME=C:\gpcdemo-v0
 xcopy "%JENKINS_WORKSPACE%\config" "%GPC_V0_HOME%\files\config" /s /y /EXCLUDE:%GPC_V0_HOME%\files\deploy\exclusions.txt
 xcopy "%JENKINS_WORKSPACE%\gpconnect-demonstrator-api\target\gpconnect-demonstrator-api.war" "%GPC_V0_HOME%" /y
 
-call powershell -Command "Get-ChildItem -Path \"%GPC_V0_HOME%\files\config\sql \" -Filter \"*.sql\" | ForEach-Object { (Get-Content $_.FullName) | ForEach-Object { $_ -replace 'gpconnect', 'gpconnect0' } | Set-Content $_.FullName } "
-call powershell -Command "Get-ChildItem -Path \"%GPC_V0_HOME%\files\config \" -Filter \"gpconnect-demonstrator-api.properties\" | ForEach-Object { (Get-Content $_.FullName) | ForEach-Object { $_ -replace 'gpconnect', 'gpconnect0' } | Set-Content $_.FullName } "
-
 net start "gpcdemo-v0"
